5cf606ff44db7f7d2e9b2296e9aea248c6f35dd6
Daniil Arkhangelsky <kiky.tokamuro@yandex.ru>
2025-03-18 15:40:06 +0300

    Fix version and register

--- src/sentry_zabbix/__init__.py
+++ src/sentry_zabbix/__init__.py
@@ -3,13 +3,14 @@
 sentry_zabbix
 """
 from __future__ import absolute_import
+from importlib.metadata import version
 
 try:
-    VERSION = __import__('pkg_resources').get_distribution(__name__).version
+    VERSION = version(__name__)
 except Exception as e:
     VERSION = 'unknown'
 
-from sentry.plugins import register
+from sentry.plugins.base import register
 from .plugin import ZabbixPlugin
 
 # register(ZabbixPlugin)
