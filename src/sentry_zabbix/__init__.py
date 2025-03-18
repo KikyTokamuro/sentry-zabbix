897d4352c50c76f14b6fc0909fef3038a25c5e5c
Daniil Arkhangelsky <kiky.tokamuro@yandex.ru>
2025-03-18 15:35:47 +0300

    Fix exception

--- src/sentry_zabbix/__init__.py
+++ src/sentry_zabbix/__init__.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import
 
 try:
     VERSION = __import__('pkg_resources').get_distribution(__name__).version
-except Exception, e:
+except Exception as e:
     VERSION = 'unknown'
 
 from sentry.plugins import register
